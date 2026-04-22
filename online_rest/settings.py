d40da111dd2f5ea0f37ea902ca72a32335839286
Oga'bek Amonov <amonovdobry07@gmail.com>
2026-04-22 17:07:52 +0500

    second work finish

--- online_rest/settings.py
+++ online_rest/settings.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
